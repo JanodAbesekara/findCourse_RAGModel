839abca470b1c8b7a87dba35245dc5bb7194bbcb
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-09-30 14:35:10 +0530

    fix

--- main.py
+++ main.py
@@ -1,34 +1,3 @@
-# from fastapi import FastAPI
-# import random
-# from pydantic import BaseModel
-
-# app = FastAPI()
-
-
-# @app.get("/")
-# async def read_root():
-#     return {"Hello": "World"}
-
-
-# @app.get("/random")
-# async def random_number():
-#     return random.randint(0, 100)
-
-
-# class DataModel(BaseModel):
-#     name: str
-#     age: int
-#     email: str
-
-# @app.post("/adddata")
-# async def add_data(data: DataModel):
-    
-    
-    
-    
-    
-#     return {"message": "Data added successfully", "data": data}
-
 
 from fastapi.middleware.cors import CORSMiddleware
 
